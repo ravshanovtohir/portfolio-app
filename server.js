91798500d6a0f9caf22d7699a974c51ff0ebb63b
Tohir Ravshanov <ravshanovtohir11@gmail.com>
2022-03-24 21:53:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const path = require("path")
 const express = require("express")
 const app = express()
-const port = 4000
+const port = process.env.PORT || 5000
 
 app.use(express.static("public"))
 
