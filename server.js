fa445f30c9fd2d520d64ce89dba4c9f1be5597e8
Tohir Ravshanov <ravshanovtohir11@gmail.com>
2022-03-24 21:39:01 +0500

    started
    server builded

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+const path = require("path")
+const express = require("express")
+const app = express()
+const port = 4000
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/main.html"))
+})
+
+app.listen(port, () => console.log("Server is running at http://localhost:" + port))
